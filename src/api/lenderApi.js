424777ec55477058804e966056309cb1a2705c1c
Pankaj Pal <pankajpal8967890080@gmail.comm>
2025-12-30 11:37:42 +0530

    lender api

--- src/api/lenderApi.js
+++ src/api/lenderApi.js
@@ -1,6 +1,6 @@
 import API from "./api";
 
 export const getLenders = async (params) => {
-  const res = await API.get("/lender/list", { params });
-  return res.data;
+    const res = await API.get("/lender/list", { params });
+    return res.data;
 };
